00fe962fdde39e4e280aac1a23f9ee532dfaa3fb
chajuy <juyun2490@gmail.com>
2026-03-21 11:26:06 +0900

    Fix：順番チェンジ

--- data/quotes.ts
+++ data/quotes.ts
@@ -1,10 +1,10 @@
 export const quotes = [
   {
-    text: "Shit first. Stardom turns everything into a masterpiece.",
+    text: "먼저 똥을 싸라. 명성은 모든 것을 걸작으로 만든다.",
     author: "KURUMA",
   },
   {
-    text: "먼저 똥을 싸라. 명성은 모든 것을 걸작으로 만든다.",
+    text: "Shit first. Stardom turns everything into a masterpiece.",
     author: "KURUMA",
   },
 ];

9a20180be15872f04302e8ec4fee139c690bd904
chajuy <juyun2490@gmail.com>
2026-03-21 17:47:28 +0900

    fix: explicitly pass env variables

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        // Cloudinary의 모든 이미지 경로를 허용
       },
     ],
   },
